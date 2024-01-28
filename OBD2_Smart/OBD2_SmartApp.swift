3d2a84d1985afda8159073dfa21ae9ec14e7a253
Ivan Dimitrov <biobeehive@abv.bg>
2024-01-28 04:54:11 +0200

    Initial Commit

---
+++ OBD2_Smart/OBD2_SmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  OBD2_SmartApp.swift
+//  OBD2_Smart
+//
+//  Created by Dosi Dimitrov on 28.01.24.
+//
+
+import SwiftUI
+
+@main
+struct OBD2_SmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
